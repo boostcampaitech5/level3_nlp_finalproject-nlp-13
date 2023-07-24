7b1d0ed500385ba0c37b882e43fd68d33cdb7743
kJS <8804who@naver.com>
2023-07-24 22:35:29 +0900

    forms 삭제

--- flask/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, EqualTo
-
-class SignInForm(FlaskForm):
-    ID = StringField("ID")
-    PW = PasswordField("PW")
-    submit = SubmitField("Sign in")
